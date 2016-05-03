b5c546047738afca2faf2d2c77a7843baba8c55c
Leeinseong <dlstjd0117@ajou.ac.kr>
2016-05-03 19:27:26 +0900

    123

--- OOP_Team8/src/RacingOnJavaRoad.java
+++ OOP_Team8/src/RacingOnJavaRoad.java
@@ -1,4 +1,8 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
 
 public class RacingOnJavaRoad {
 
+	
 }
---
+++ OOP_Team8/src/User.java
@@ -0,0 +1,9 @@
+
+public class User {
+
+	int money;
+	int id;
+	String password = new String();
+	int record;
+	
+}
